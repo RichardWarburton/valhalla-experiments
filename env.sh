b8d82ff0e5b5ad5b30191c413e2e047e0a40d0a7
Richard Warburton <richard.warburton@gmail.com>
2015-01-17 14:07:59 +0000

    oop

--- env.sh
+++ env.sh
@@ -4,7 +4,7 @@ set -eu
 
 export JAVA_HOME=/home/richard/Projects/valhalla/build/linux-x86_64-normal-server-release/images/j2sdk-image/
 export PATH=$JAVA_HOME/bin:$PATH
-export LD_LIBRARY_PRELOAD=$PWD
+export LD_LIBRARY_PATH=$PWD
 
 export compilation="-XX:+LogCompilation"
 export trace="-XX:+TraceClassLoading"
